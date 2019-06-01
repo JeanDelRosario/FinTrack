dae2716caa252db5fcef00204c565c4d2f3d160a
JeanDelRosario <jeancarlos18_09@hotmail.com>
2019-06-01 19:49:15 -0400

    M

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
-});
+})
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
