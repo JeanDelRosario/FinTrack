9001e22ddc1193132943985dd4807ee10dbf171b
JeanDelRosario <jeancarlos18_09@hotmail.com>
2019-06-01 21:50:40 -0400

    Started serving build folder

--- server.js
+++ server.js
@@ -15,13 +15,17 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.use(express.static('build', { index: false }));
+
 const connection = mysql.createPool({
     connectionLimit: 10,
     host: process.env.HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
-})
+});
+
+console.log('Connected to database');
 
 routes(app, connection);
 app.listen(process.env.PORT);
