1e00ed315373561ce2a8cac4f4faa7e59e34cff1
JeanDelRosario <jeancarlos18_09@hotmail.com>
2019-06-01 20:52:55 -0400

    Took off the connection pool, now trying taking the throw error with a console log

--- server.js
+++ server.js
@@ -15,15 +15,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-const connection = mysql.createPool({
-    connectionLimit: 10,
+const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
 })
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) console.log(err);
     console.log('Connected!');
     routes(app, connection);
     app.listen(process.env.PORT);
