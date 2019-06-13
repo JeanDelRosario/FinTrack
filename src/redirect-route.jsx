801942c8c43e78724c3f1a95b4852c59bbd2b0d9
JeanDelRosario <jeancarlos18_09@hotmail.com>
2019-06-12 20:44:03 -0400

    Same problem 2

--- src/redirect-route.jsx
+++ src/redirect-route.jsx
@@ -4,6 +4,8 @@ import { withRouter } from 'react-router-dom';
 class RedirectRoute extends Component {
 
     componentDidMount() {
+        console.log('hi')
+        console.log(this.props.location.pathname )
         if((this.props.location.pathname === '/') | (this.props.location.pathname === '')) this.props.history.push('/dashboard/analytics');
     }
     render() {
